f799524b3466c18cefeb3aa9fc848f8c048e9fc2
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-31 12:35:14 -0600

    new strcuture as While Loop

--- script.py
+++ script.py
@@ -4,14 +4,26 @@ import os
 
 ###--- FUNCTIONS ---###
 def turn_device_off():
-    turn_off = input('Wanna turn this laptop off?\n')
-
-    if turn_off == 'no':
-        exit()
-    elif turn_off == 'yes':
-        os.system('sudo shutdown -r now')
-    # else:
-    #     print('sorry, dunno what tha means...')
+    '''
+     user has 3 attempts to turn laptop off.
+     if user says "n" or "no", loop breaks
+     if user says "y" or "yes", command is executed and device is turned off
+     else, the loop starts over 2 more times
+    '''
+    attempts = 0
+    while attempts < 3:
+        question = '🤖: wanna turn this laptop off? '
+        turn_off = input(question)
+        if turn_off == 'no' or turn_off == 'n':
+            print('🤖: oh, ok -- bye!')
+            exit()
+        elif turn_off == 'yes' or turn_off == 'y':
+            print('🤖: coolio, enter your password and i\'ll do the rest.')
+            os.system('sudo shutdown -r now')
+        else:
+            print('🤖: sorry, dunno what tha means...')
+            attempts += 1
+    print('🤖: i\'m bored... see ya!')
 
 
 ###--- DRIVER CODE ---###
